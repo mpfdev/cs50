739b59d622654a38f6bc0dda94f1083e016f03df
mpfdev <matheus.filgueiras17@gmail.com>
2022-07-25 16:14:25 -0300

    init filter: greyscale

---
+++ pset4/Filter/helpers.c
@@ -0,0 +1,33 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+            double averageColor = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0;
+            image[i][j].rgbtRed = (int) round(averageColor);
+            image[i][j].rgbtGreen = (int) round(averageColor);
+            image[i][j].rgbtBlue = (int) round(averageColor);
+        }
+    }
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
